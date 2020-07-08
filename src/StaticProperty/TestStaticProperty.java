630a2a68ceba810b908d455374dca922b43737cf
huy8895 <huy8895@gmail.com>
2020-07-08 22:28:28 +0700

    [Thực hành] Static Property

---
+++ src/StaticProperty/TestStaticProperty.java
@@ -0,0 +1,14 @@
+package StaticProperty;
+
+public class TestStaticProperty {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda 3","SkyActive 3");
+        System.out.println(Car.numberOfCars);
+        Car car2 = new Car("Mazda 4","SkyActive 4");
+        System.out.println(Car.numberOfCars);
+
+        Car car3 = new Car("Mazda 5","SkyActive 5");
+        System.out.println(Car.numberOfCars);
+
+    }
+}
