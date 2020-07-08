73eaabb1d9fdbba0c374de300e34172093a4d03f
huy8895 <huy8895@gmail.com>
2020-07-08 22:39:03 +0700

    [Bài tập] Access modifier

---
+++ src/Access_modifier/TestCircle.java
@@ -0,0 +1,8 @@
+package Access_modifier;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10);
+        System.out.println("Area : " + circle.getArea() + " Radius: " + circle.getRadius());
+    }
+}
