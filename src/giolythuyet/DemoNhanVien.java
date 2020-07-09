8f049f6b6f4c5a5f62fc3a12913e4238206aaac6
huy8895 <huy8895@gmail.com>
2020-07-09 15:52:01 +0700

    bai thuc hanh gio ly thuyet

---
+++ src/giolythuyet/DemoNhanVien.java
@@ -0,0 +1,8 @@
+package giolythuyet;
+
+public class DemoNhanVien {
+    public static void main(String[] args) {
+        Nhanvien nhanvien = new Nhanvien(8895,"huy","C0520K1");
+        System.out.println(nhanvien);
+    }
+}
