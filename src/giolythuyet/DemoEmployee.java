01875ece013116f1f763f46715763dc92bc3f552
huy8895 <huy8895@gmail.com>
2020-07-09 15:56:38 +0700

    sua ten thanh tieng anh

---
+++ src/giolythuyet/DemoEmployee.java
@@ -0,0 +1,8 @@
+package giolythuyet;
+
+public class DemoEmployee {
+    public static void main(String[] args) {
+        Employee employee = new Employee(8895,"huy","C0520K1");
+        System.out.println(employee);
+    }
+}
--- src/giolythuyet/DemoNhanVien.java
+++
@@ -1,8 +0,0 @@
-package giolythuyet;
-
-public class DemoNhanVien {
-    public static void main(String[] args) {
-        Nhanvien nhanvien = new Nhanvien(8895,"huy","C0520K1");
-        System.out.println(nhanvien);
-    }
-}
